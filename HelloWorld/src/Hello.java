64fa6cfd6be51beacdc48193df661c2c97c548b2
kishore008 <kishoresminds@gmail.com>
2019-06-19 19:49:18 +0530

    What Are Variables

--- HelloWorld/src/Hello.java
+++ HelloWorld/src/Hello.java
@@ -1,5 +1,14 @@
 public class Hello {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
+
+        int myFirstNumber = 20;
+        int mySecondNumber = 8;
+        int myThirdNumber = 07;
+
+        int myTotal = myFirstNumber + mySecondNumber + myThirdNumber;
+
+        int myLastOne = 1000 - myTotal;
+        System.out.println(myLastOne);
     }
 }
