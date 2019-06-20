1405f6fe0bce5e3f1c45b09dde213110d1afb81f
kishore008 <kishoresminds@gmail.com>
2019-06-20 17:46:36 +0530

    7. Getting To Know Primitive Data Types - Char And Boolean

---
+++ CharAndBoolean/src/com/kishore/Main.java
@@ -0,0 +1,15 @@
+package com.kishore;
+
+public class Main {
+
+    public static void main(String[] args) {
+	    char myChar = '\u00A9';
+        System.out.println("Unicode output was: " + myChar);
+
+        boolean myBoolean = false;
+        boolean isMale = true;
+
+        char registerdUnicode = '\u00AE';
+        System.out.println("Unicode output was: " + registerdUnicode);
+    }
+}
