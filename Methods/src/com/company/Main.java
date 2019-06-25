910ead3822471dbef8ac4902f8e12d2f36e8d571
kishore008 <kishoresminds@gmail.com>
2019-06-25 12:05:29 +0530

    4.8 Method Challenge - Final Code Changes

--- Methods/src/com/company/Main.java
+++ Methods/src/com/company/Main.java
@@ -6,45 +6,45 @@ public class Main {
 
         int highScore = calculateScore(true, 100, 4, 100);
         int position = calculateHighScorePosition(highScore);
-        displayHighScorePosition("kishore",position );
+        displayHighScorePosition("kishore", position);
 
         highScore = calculateScore(true, -100, 0, 100);
         position = calculateHighScorePosition(highScore);
-        displayHighScorePosition("Madhuri",position );
+        displayHighScorePosition("Madhuri", position);
 
         highScore = calculateScore(true, -600, 0, 100);
         position = calculateHighScorePosition(highScore);
-        displayHighScorePosition("Sunny",position );
+        displayHighScorePosition("Sunny", position);
 
         highScore = calculateScore(true, -950, 0, 100);
         position = calculateHighScorePosition(highScore);
-        displayHighScorePosition("Aandal Amma",position );
+        displayHighScorePosition("Aandal Amma", position);
     }
 
-    public static int calculateScore(boolean gameOver, int score, int levelCompleted, int bonus){
+    public static int calculateScore(boolean gameOver, int score, int levelCompleted, int bonus) {
 
-        if (gameOver){
+        if (gameOver) {
             int finalScore = score + (levelCompleted * bonus);
             finalScore += 1000;
-            return  finalScore;
+            return finalScore;
         }
         return -1;
     }
 
-    public static int calculateHighScorePosition(int highScore){
-        if(highScore>=1000){
-            return 1;
+    public static int calculateHighScorePosition(int highScore) {
+        int position = 4;
+
+        if (highScore >= 1000) {
+            position = 1;
+        } else if (highScore >= 500) {
+            position = 2;
+        } else if (highScore >= 100) {
+            position = 3;
         }
-        else if(highScore>=500 && highScore <1000){
-            return 2;
-        }
-        else if(highScore>=100 && highScore<500){
-            return 3;
-        }
-        return 4;
+        return position;
     }
 
-    public static void displayHighScorePosition(String name, int position){
+    public static void displayHighScorePosition(String name, int position) {
         System.out.println(name + " managed to get into position " + position + " on the high score table");
     }
 }
