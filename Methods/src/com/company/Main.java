fe73f5e6f595951993018f288fc3b43ebebfb227
kishore008 <kishoresminds@gmail.com>
2019-06-25 11:56:21 +0530

    4.7 More On Methods And A Challenge

--- Methods/src/com/company/Main.java
+++ Methods/src/com/company/Main.java
@@ -4,9 +4,21 @@ public class Main {
 
     public static void main(String[] args) {
 
-        calculateScore(true, 800, 5, 100);
+        int highScore = calculateScore(true, 100, 4, 100);
+        int position = calculateHighScorePosition(highScore);
+        displayHighScorePosition("kishore",position );
 
-        calculateScore(true, 10000, 8, 200);
+        highScore = calculateScore(true, -100, 0, 100);
+        position = calculateHighScorePosition(highScore);
+        displayHighScorePosition("Madhuri",position );
+
+        highScore = calculateScore(true, -600, 0, 100);
+        position = calculateHighScorePosition(highScore);
+        displayHighScorePosition("Sunny",position );
+
+        highScore = calculateScore(true, -950, 0, 100);
+        position = calculateHighScorePosition(highScore);
+        displayHighScorePosition("Aandal Amma",position );
     }
 
     public static int calculateScore(boolean gameOver, int score, int levelCompleted, int bonus){
@@ -14,9 +26,25 @@ public class Main {
         if (gameOver){
             int finalScore = score + (levelCompleted * bonus);
             finalScore += 1000;
-            System.out.println("Your final score was " + finalScore);
             return  finalScore;
         }
         return -1;
+    }
+
+    public static int calculateHighScorePosition(int highScore){
+        if(highScore>=1000){
+            return 1;
+        }
+        else if(highScore>=500 && highScore <1000){
+            return 2;
+        }
+        else if(highScore>=100 && highScore<500){
+            return 3;
+        }
+        return 4;
+    }
+
+    public static void displayHighScorePosition(String name, int position){
+        System.out.println(name + " managed to get into position " + position + " on the high score table");
     }
 }
