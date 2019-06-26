8c1e3a4d9eb07a6d94bf08d68514ad87ef5bc851
kishore008 <kishoresminds@gmail.com>
2019-06-26 18:14:15 +0530

    4.15 Coding Exercises Example

---
+++ speedConverter/src/com/kishore/Main.java
@@ -0,0 +1,11 @@
+package com.kishore;
+
+public class Main {
+
+    public static void main(String[] args) {
+	    long miles = speedConverter.toMilesPerHour(10.5);
+        System.out.println("Miles = " + miles);
+
+        speedConverter.printConversion(10.5);
+    }
+}
