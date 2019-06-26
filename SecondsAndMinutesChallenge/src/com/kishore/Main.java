798670d0267126b3b83069db6778b565b834b125
kishore008 <kishoresminds@gmail.com>
2019-06-26 20:36:14 +0530

    4.25 Seconds and Minutes Challenge

---
+++ SecondsAndMinutesChallenge/src/com/kishore/Main.java
@@ -0,0 +1,30 @@
+package com.kishore;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println(getDurationString(65,45));
+        System.out.println(getDurationString(3945));
+    }
+
+    public static String getDurationString(int minutes, int seconds){
+        if((minutes<0) || (seconds<0 || seconds>59)){
+            return "Invalid Value";
+        }
+        int hours = minutes/60;
+        int remainingMinutes = minutes%60;
+
+
+        return (hours + "h " + remainingMinutes + "m " + seconds + "s");
+    }
+
+    public static String getDurationString(int seconds){
+        if(seconds<0){
+            return "Invalid Value";
+        }
+        int minutesInSeconds = seconds/60;
+        int reminingSeconds = seconds%60;
+
+        return getDurationString(minutesInSeconds, reminingSeconds);
+    }
+}
