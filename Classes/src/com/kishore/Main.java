66c037233315d4d33992b70ff6edc8f9f949c627
kishore008 <kishoresminds@gmail.com>
2019-07-04 17:46:34 +0530

    6.3 Classes part 1 and 2

---
+++ Classes/src/com/kishore/Main.java
@@ -0,0 +1,13 @@
+package com.kishore;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        Car porsche = new Car();
+        Car holden = new Car();
+
+        porsche.setModel("carrera");
+        System.out.println("Model is " + porsche.getModel());
+    }
+}
